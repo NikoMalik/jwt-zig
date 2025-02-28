be9d92d46020bf79d930885ed32537d6434cfc0f
NikoMalik <123812634+NikoMalik@users.noreply.github.com>
2025-02-28 22:52:26 +0300

    [fix] I will burn this planet down before i spend another minute among these animals

--- build.zig
+++ build.zig
@@ -69,6 +69,7 @@ pub fn build(b: *std.Build) void {
     if (builtin.target.os.tag == .windows) {
         lib_mod.linkSystemLibrary("libssl-3-x64", .{});
         lib_mod.linkSystemLibrary("libcrypto-3-x64", .{});
+        lib_mod.addSystemIncludePath(.{ .cwd_relative = "C:\\Program Files\\OpenSSL\\include" });
         lib_mod.addLibraryPath(.{ .cwd_relative = "C:\\Program Files\\OpenSSL\\bin" });
         lib.addLibraryPath(.{ .cwd_relative = "C:\\Program Files\\OpenSSL\\bin" });
         lib.linkSystemLibrary("libssl-3-x64");
@@ -87,17 +88,16 @@ pub fn build(b: *std.Build) void {
 
         rsa_test.linkSystemLibrary("libssl-3-x64");
         rsa_test.linkSystemLibrary("libcrypto-3-x64");
+        rsa_test.addLibraryPath(.{ .cwd_relative = "C:\\Program Files\\OpenSSL\\bin" });
         rsa_test.linkLibC();
 
         jwt_test.linkSystemLibrary("libssl-3-x64");
         jwt_test.linkSystemLibrary("libcrypto-3-x64");
+        jwt_test.addLibraryPath(.{ .cwd_relative = "C:\\Program Files\\OpenSSL\\bin" });
         jwt_test.linkLibC();
 
         unit_tests_2.addLibraryPath(.{ .cwd_relative = "C:\\Program Files\\OpenSSL\\bin" });
         unit_tests_2.linkLibC();
-
-        rsa_test.addLibraryPath(.{ .cwd_relative = "C:\\Program Files\\OpenSSL\\bin" });
-        jwt_test.addLibraryPath(.{ .cwd_relative = "C:\\Program Files\\OpenSSL\\bin" });
     } else {
         lib_mod.linkSystemLibrary("ssl", .{});
         lib_mod.linkSystemLibrary("crypto", .{});
