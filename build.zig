25d4b2cddf741068243bf60fb3e6bfdc3866048b
NikoMalik <123812634+NikoMalik@users.noreply.github.com>
2025-03-01 18:40:39 +0300

    [fix] I will burn this planet down before i spend another minute among these animals

--- build.zig
+++ build.zig
@@ -66,25 +66,60 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .link_libc = true,
     });
-    if (builtin.os.tag == .windows) {
-        const openssl_path = std.os.getenv("OPENSSL_ROOT_DIR") orelse "C:/Program Files/OpenSSL";
+    if (builtin.target.os.tag == .windows) {
+        lib_mod.linkSystemLibrary("libssl-3-x64", .{});
+        lib_mod.linkSystemLibrary("libcrypto-3-x64", .{});
 
-        lib_mod.addIncludePath(.{ .cwd_relative = openssl_path ++ "/include" });
+        lib_mod.addLibraryPath(.{ .cwd_relative = "C:/Program Files/OpenSSL/bin" });
+        lib.addLibraryPath(.{ .cwd_relative = "C:/Program Files/OpenSSL/bin" });
+        lib.linkSystemLibrary("libssl-3-x64");
+        lib.linkSystemLibrary("libcrypto-3-x64");
+        lib.linkSystemLibrary("crypt32");
+        lib.linkSystemLibrary("ws2_32");
+        lib.linkSystemLibrary("advapi32");
+        lib_mod.linkSystemLibrary("crypt32", .{});
+        lib_mod.linkSystemLibrary("ws2_32", .{});
+        lib_mod.linkSystemLibrary("advapi32", .{});
+        lib.linkLibC();
 
-        lib_mod.addLibraryPath(.{ .cwd_relative = openssl_path ++ "/lib" });
-        lib_mod.addLibraryPath(.{ .cwd_relative = openssl_path ++ "/bin" });
+        unit_tests_1.linkSystemLibrary("libssl-3-x64");
+        unit_tests_1.linkSystemLibrary("libcrypto-3-x64");
+        unit_tests_1.addLibraryPath(.{ .cwd_relative = "C:/Program Files/OpenSSL/bin" });
+        unit_tests_1.linkSystemLibrary("crypt32");
+        unit_tests_1.linkSystemLibrary("ws2_32");
+        unit_tests_1.linkSystemLibrary("advapi32");
+        unit_tests_1.linkLibC();
 
-        const win_libs = [_][]const u8{ "libssl-3-x64", "libcrypto-3-x64", "crypt32", "ws2_32", "advapi32", "user32" };
+        unit_tests_3.linkSystemLibrary("libssl-3-x64");
+        unit_tests_3.linkSystemLibrary("libcrypto-3-x64");
+        unit_tests_3.addLibraryPath(.{ .cwd_relative = "C:/Program Files/OpenSSL/bin" });
+        unit_tests_3.linkSystemLibrary("crypt32");
+        unit_tests_3.linkSystemLibrary("ws2_32");
+        unit_tests_3.linkSystemLibrary("advapi32");
 
-        inline for (win_libs) |lib_name| {
-            lib_mod.linkSystemLibrary(lib_name, .{});
-            jwt_test.linkSystemLibrary(lib_name);
-            rsa_test.linkSystemLibrary(lib_name);
-            unit_tests_1.linkSystemLibrary(lib_name);
-            unit_tests_2.linkSystemLibrary(lib_name);
-            lib.linkSystemLibrary(lib_name);
-            unit_tests_3.linkSystemLibrary(lib_name);
-        }
+        unit_tests_3.linkLibC();
+
+        rsa_test.linkSystemLibrary("libssl-3-x64");
+        rsa_test.linkSystemLibrary("libcrypto-3-x64");
+        rsa_test.addLibraryPath(.{ .cwd_relative = "C:/Program Files/OpenSSL/bin" });
+        rsa_test.linkSystemLibrary("crypt32");
+        rsa_test.linkSystemLibrary("ws2_32");
+        rsa_test.linkSystemLibrary("advapi32");
+        rsa_test.linkLibC();
+
+        jwt_test.linkSystemLibrary("libssl-3-x64");
+        jwt_test.linkSystemLibrary("libcrypto-3-x64");
+        jwt_test.addLibraryPath(.{ .cwd_relative = "C:/Program Files/OpenSSL/bin" });
+        jwt_test.linkSystemLibrary("crypt32");
+        jwt_test.linkSystemLibrary("ws2_32");
+        jwt_test.linkSystemLibrary("advapi32");
+        jwt_test.linkLibC();
+
+        unit_tests_2.addLibraryPath(.{ .cwd_relative = "C:/Program Files/OpenSSL/bin" });
+        unit_tests_2.linkSystemLibrary("crypt32");
+        unit_tests_2.linkSystemLibrary("ws2_32");
+        unit_tests_2.linkSystemLibrary("advapi32");
+        unit_tests_2.linkLibC();
     } else {
         lib_mod.linkSystemLibrary("ssl", .{});
         lib_mod.linkSystemLibrary("crypto", .{});
